9a22a903d07e35ef97c2bd783fd3dfd2a556decc
IEatCrayon <74836936+IEatCrayon@users.noreply.github.com>
2022-03-12 14:14:36 -0500

    Initial Commit

---
+++ Creating Flashcard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Creating Flashcard
+//
+//  Created by Kevin Li on 3/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
